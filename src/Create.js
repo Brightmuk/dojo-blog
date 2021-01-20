b8b274d669ebef6b32354d40c4b9f3f263188828
Brightmuk <be.rightmuk@gmail.com>
2021-01-20 22:18:58 +0300

    Add routing functionality

---
+++ src/Create.js
@@ -0,0 +1,10 @@
+const Create = () => {
+    return ( 
+        <div className="create">
+            <h2 className="text-center">New blog</h2>
+
+        </div>
+     );
+}
+ 
+export default Create;
--- src/home.js
+++ src/home.js
@@ -1,39 +1,19 @@
 import {useState, useEffect} from 'react';
 import BlogList from './BlogList';
 import Loader from './Loader';
+import useFetch from './UseFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [error, setError] = useState(null);
-
+    const {data:blogs, isPending, error} = useFetch('  http://localhost:8000/blogs');
     const handleDelete = (id)=>{
-      const newBlogs = blogs.filter(blog=>blog.id!==id)
-      setBlogs(newBlogs);
+      // const newBlogs = blogs.filter(blog=>blog.id!==id)
+      // setBlogs(newBlogs);
     }
-    useEffect(()=>{
-     fetch(' http://localhost:8000/blogs')
-     .then(res=>{
-        if(!res.ok){
-          throw Error('An error occured, try again later.')
-        }
-        return res.json()
-     })
-     .then(data=>{
-        console.log(data);
-        setBlogs(data)
-        setError(null);
-     })
-     .catch(err=>{
-      setError(err.message);
-      setBlogs([]);
-     })
-    },[])
 
     return ( 
         <div className="home">
            {error && <div className="text-center danger-text">{error}</div>}
-          { 
-         blogs!=null?
+          { blogs!=null?
            <BlogList blogs={blogs} title="Today's blogs" handleDelete={handleDelete}/>
            :<Loader/>
           }
