d4f250b987df79c41933f8864df93a96b9857999
Brightmuk <be.rightmuk@gmail.com>
2021-01-21 23:59:46 +0300

    Add post method to create new items

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs,title,handleDelete}) => {
 
     return ( 
@@ -5,14 +7,16 @@ const BlogList = ({blogs,title,handleDelete}) => {
             <h2 className="text-center">{title}</h2>
             <br/>
              {blogs.map((blog)=>(
+                 <Link to={"blogs/"+blog.id}>
               <div className="blog-preview" key={blog.id}>
                  <h3>{blog.title}</h3>
-                 <p> by {blog.author}</p>
+                 <p className="author"> by {blog.author}</p>
                  <div className="blog-preview-footer">
                      <div  className="post-time">posted {blog.postTime}</div>
                      <button onClick={()=>handleDelete(blog.id)}>Delete</button>
                  </div>
               </div>
+              </Link>
            ))}
         </div>
      );
